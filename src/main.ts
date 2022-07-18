1ce24b6f75c85826622b1d4d12715a673e749e24
Gabriel Zulian <zulian@MacBook-Air-de-Gabriel.local>
2022-07-17 23:50:56 -0400

    test3

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-//if (environment.production) {
+if (environment.production) {
   enableProdMode();
-//}
-
-var admin = require("firebase-admin");
-
-var serviceAccount = require("heyandes-test-gzulian-firebase-adminsdk-4dulc-0bf4e12126.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
